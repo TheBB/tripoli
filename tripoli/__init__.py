71eadb8d4edb5fded7b8bd9980fca3b3e71eefd5
Eivind Fonn <evfonn@gmail.com>
2016-11-07 09:57:28 +0100

    Messages

--- tripoli/__init__.py
+++ tripoli/__init__.py
@@ -4,5 +4,9 @@ try:
     functionp = emacs.intern('functionp')
     assert functionp(emacs.intern('intern'))
     assert not functionp(emacs.intern('nil'))
+
+    emacs.message('Tripoli successfully loaded')
 except Exception as e:
     print('{}: {}'.format(e.__class__.__name__, e))
+
+print('Tripoli successfully loaded')
