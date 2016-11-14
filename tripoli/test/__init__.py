a49a839e1001dc325af81c6028cf7e2f1acf0e75
Eivind Fonn <evfonn@gmail.com>
2016-11-14 14:32:00 +0100

    Refactor test suite

---
+++ tripoli/test/__init__.py
@@ -0,0 +1,24 @@
+from os.path import abspath, dirname, join
+import pytest
+import sys
+
+
+def run_tests():
+    try:
+        # Check that we are running embedded
+        import emacs_raw
+    except ImportError:
+        print("Can't run tests outside Emacs embedding.")
+    else:
+
+        try:
+            # Fool pytest into running
+            sys.argv = ['tripoli']
+
+            module = abspath(join(dirname(__file__), '..'))
+            pytest.main(['-v', module])
+
+        except Exception as e:
+            print('{}: {}'.format(e.__class__.__name__, e))
+            _, _, tb = sys.exc_info()
+            traceback.print_tb(tb)
