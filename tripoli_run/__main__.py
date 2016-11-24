85f2c74c698862cf190b96935bd3a10afd6de611
Eivind Fonn <evfonn@gmail.com>
2016-11-24 20:13:12 +0100

    Drop into shell embedded
    Couldn’t get ipython or bpython to work

--- tripoli_run/__main__.py
+++ tripoli_run/__main__.py
@@ -12,6 +12,9 @@ def main():
         args.insert(1, '--batch')
         args.extend(['--eval', '(tripoli-run-string "from tripoli.test import run_tests; run_tests()")',
                      '--eval', '(kill-emacs)'])
+    elif '--shell' in sys.argv:
+        args.insert(1, '--batch')
+        args.extend(['--eval', '(tripoli-run-string "import code; c = code.InteractiveConsole(); c.push(\'import tripoli\'); c.interact()")'])
     else:
         dotfile = join(dirname(__file__), '..', 'dotemacs.py')
         args.extend(['--eval', '(tripoli-run-file "{}")'.format(dotfile)])
