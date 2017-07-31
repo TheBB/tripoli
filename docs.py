268f36016e01e41b166768f10d2e2085291fdd7a
Eivind Fonn <evfonn@gmail.com>
2017-07-31 13:59:44 +0200

    Inhibit init when making docs

--- docs.py
+++ docs.py
@@ -10,12 +10,16 @@ if __name__ == '__main__':
     cwd = os.getcwd()
     docdir = join(cwd, '..', 'docs')
 
-    args = ['emacs', '-q', '--batch', '-L', cwd, '-l', 'libtripoli']
     code = textwrap.dedent("""
     import sys
     sys.argv = []
     from sphinx import main
     sys.exit(main(['sphinx-build', '-b', 'html', '-d', 'build/doctrees', 'source', 'build/html']))
     """)
-    args.extend(['--eval', '(tripoli-exec-str "{}")'.format(code)])
+    args = [
+        'emacs', '-q', '--batch', '-L', cwd,
+        '--eval', '(setq tripoli-inhibit-init t)',
+        '-l', 'libtripoli',
+        '--eval', '(tripoli-exec-str "{}")'.format(code)
+    ]
     subprocess.run(args, check=True, cwd=docdir)
